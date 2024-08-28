3bc63a0e346811194f8603306ec3e9cff6e5b9db
Arthur <arthurdragon39@gmail.com>
2024-08-27 20:28:22 -0400

    commit

--- realestatereactasp.net.client/src/PropertyForm.jsx
+++ realestatereactasp.net.client/src/PropertyForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './styles.css';
 
 const PropertyForm = () => {
     const [title, setTitle] = useState('');
